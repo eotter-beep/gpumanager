3827ba2f53ff5a75a8db9000b3023ed5cfa88030
eotter-beep <eotter76@outlook.com>
2025-09-22 12:29:08 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import os
+import GPUtil
+import platform
 
 def run1():
     script_path = r"restartgpu.py"
@@ -7,7 +9,16 @@ def run1():
 
 root = tk.Tk()
 root.title("GPUManager")
-
+info = f"System: {platform.system()}\n \
+User Name: {platform.node()}\n \
+Release: {platform.release()}\n \
+Version: {platform.version()}\n \
+Machine: {platform.machine()}\n \
+Processor: {platform.processor()}\n \
+Python Version: {platform.python_version()}\n \
+GPU: {gpus[0].name}\n \
+"
+Label(root, text=info, font=("Helvetica", 14))
 button = tk.Button(root, text="Restart GPU", command=run1, height=2, width=20)
 button.pack(padx=20, pady=20)
 
