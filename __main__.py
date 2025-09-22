c9046246c1d75390087f5bd9bd2ce9cc03a284fa
eotter-beep <eotter76@outlook.com>
2025-09-22 12:16:39 -0700

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import os
+
+def run1():
+    script_path = r"C:\path\to\yourscript.py"
+    os.system(f'start cmd /k python "{script_path}"')
+
+root = tk.Tk()
+root.title("GPUManager")
+
+button = tk.Button(root, text="Restart GPU", command=run1, height=2, width=20)
+button.pack(padx=20, pady=20)
+
+root.mainloop()
