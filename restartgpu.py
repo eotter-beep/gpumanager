eb6b73bbb1975a2d58a250f6c06eff05c1b9bfc9
eotter-beep <eotter76@outlook.com>
2025-09-22 12:21:30 -0700

    Update restartgpu.py

--- restartgpu.py
+++ restartgpu.py
@@ -4,9 +4,9 @@ import time
 print("GPUManager")
 print("----------------------------------------------------------------------")
 print("Restart the terminal to avoid terminal messups when restarting the GPU")
-time.sleep(60)
+time.sleep(10)
 os.system('cls')
-time.sleep(60)
+time.sleep(10)
 print("Restart GPU")
 def resgpu():
   keyboard.send("windows+ctrl+shift+b")
