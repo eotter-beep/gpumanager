d7a2a1dc12d3d9d26ff41adb856deb36bf340308
eotter-beep <eotter76@outlook.com>
2025-09-23 15:33:58 -0700

    Update installer.py

--- installer.py
+++ installer.py
@@ -6,10 +6,10 @@ def startinstall():
     os.system('start cmd /k "echo Skipping the other libraries because they are included in the official Python base"')
 
 root = tk.Tk()
-root.title("GPUManager")
+root.title("DeviceManager")
 
 # Label
-label = tk.Label(root, text="Easily manage your GPU and your hardware!", font=("Helvetica", 14))
+label = tk.Label(root, text="Easily manage your devices!", font=("Helvetica", 14))
 label.pack(pady=20)
 
 # Button
